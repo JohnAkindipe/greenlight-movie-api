dde0668a77e3298dafd758c12a1cbb6c50f1c3fc
John Akindipe <akindipejohn2017@gmail.com>
2025-12-10 07:18:10 +0100

    version number incuding time

--- internal/vcs/vcs.go
+++ internal/vcs/vcs.go
@@ -8,12 +8,17 @@ import (
 //Return the version of the application (based on the git commit hash) and whether it has been
 //modified since last commit.
 func Version() string {
-    var revision string
-    var modified bool
+    var (
+        time     string
+        revision string
+        modified bool
+    )
     bi, ok := debug.ReadBuildInfo()
     if ok {
         for _, s := range bi.Settings {
             switch s.Key {
+            case "vcs.time":
+                time = s.Value
             case "vcs.revision":
                 revision = s.Value
             case "vcs.modified":
@@ -24,8 +29,7 @@ func Version() string {
         }
     }
     if modified {
-        return fmt.Sprintf("%s-dirty", revision)
+        return fmt.Sprintf("%s-%s-dirty", time, revision)
     }
-    return revision
- }
- 
+    return fmt.Sprintf("%s-%s", time, revision)
+}
